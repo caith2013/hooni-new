b3c649ec3be4331b7290c98bf792761fd9e9df6f
thomas <caith2013@gmail.com>
2026-09-11 13:12:17 -0400

    create rest example

--- src/main/java/com/hooni/controller/SolutionController.java
+++ src/main/java/com/hooni/controller/SolutionController.java
@@ -1,41 +1,52 @@
 package com.hooni.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 @Controller
 public class SolutionController
 {
-    @PostMapping("/solution")
+    @PostMapping(value = "/solution", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public ResponseEntity postSolution() {
+    public ResponseEntity<Void> postSolution() {
         return ResponseEntity.status(405).build();
     }
-    @GetMapping("/solution")
+
+    @GetMapping(value = "/solution", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public ResponseEntity<String> getSolution(@RequestParam(value = "format", required = false) String format) {
+    public ResponseEntity<Map<String, Object>> getSolution(@RequestParam(value = "format", required = false) String format) {
         if (format == null) {
             return ResponseEntity.status(400).build();
         }
         if (format.equalsIgnoreCase("short"))
         {
-            return ResponseEntity.ok(Map.of("status", "ok").toString());
-        } else if (format.equalsIgnoreCase("full")) {
-            return ResponseEntity.ok(Map.of("status", "ok", "currentTime", System.currentTimeMillis()).toString());
+            return ResponseEntity.ok(Map.of("status", "ok"));
+        }
+
+        if (format.equalsIgnoreCase("full")) {
+            return ResponseEntity.ok(
+                    Map.of(
+                            "status", "ok",
+                            "currentTime", ZonedDateTime.now().format(DateTimeFormatter.ISO_ZONED_DATE_TIME)
+                    )
+            );
         }
         return ResponseEntity.status(400).build();
     }
-    @DeleteMapping("/solution")
+    @DeleteMapping(value = "/solution", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public ResponseEntity deleteSolution() {
+    public ResponseEntity<Void> deleteSolution() {
         return ResponseEntity.status(405).build();
     }
-    @PutMapping("/solution")
+    @PutMapping(value = "/solution", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public ResponseEntity putSolution() {
+    public ResponseEntity<Void> putSolution() {
         return ResponseEntity.status(405).build();
     }
 
